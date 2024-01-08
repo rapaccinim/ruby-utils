a936ea70ef13904c352ed96d21a91d8a0328d7b7
Marco Rapaccini <info@marcorapaccini.it>
2024-01-08 11:44:09 +0000

    Simplifying code in brevo_csv.rb

--- brevo_csv/brevo_csv.rb
+++ brevo_csv/brevo_csv.rb
@@ -2,16 +2,15 @@ require 'csv'
 
 input_file = "input.csv"
 output_file = "brevo_import.csv"
-
 output_headers = %w[COMPANY EMAIL PHONE ADDRESS]
 
 def phone_formatter(phone)
   '+44' + phone.gsub(/^0|\D/,'')
 end
 
-File.write(output_file, output_headers.to_csv, mode: 'a')
-
+out = File.open(output_file, 'w')
+out << output_headers.to_csv
 CSV.read(input_file, headers: true).each do |r|
-  new_row = [r['COMPANY'], r['EMAIL'], phone_formatter(r['PHONE']), r['ADDRESS']].each(&:to_s)
-  File.write(output_file, new_row.to_csv, mode: 'a')
+  out << [r['COMPANY'], r['EMAIL'], phone_formatter(r['PHONE']), r['ADDRESS']].to_csv
 end
+out.close
