45352ec1b32424afce2359aff4ee939403d7c245
Marco <marco@webproduct.dev>
2025-07-15 11:27:59 +0100

    WIP: improved code. TBD: error handling, config in separate file

--- json_data_extractor/json_data_extractor.rb
+++ json_data_extractor/json_data_extractor.rb
@@ -2,30 +2,36 @@ require 'json'
 require 'csv'
 
 # config
-data_dir = 'dc-prospects'
-json_files_filter = "#{data_dir}/*.json"
-hash_keys = ['elements', 'fullName', 'currentPositions', 'companyName', 'title', 'entityUrn'] 
-linkedin_URL = 'https://www.linkedin.com/in/'
-regex = /fs_salesProfile:\(([^,]+)/
-out_file = 'prospects.csv'
+DATA_DIR = 'dc-prospects'
+JSON_FILES_FILTER = "#{DATA_DIR}/*.json"
+KEYS = ['elements', 'fullName', 'currentPositions', 'companyName', 'title', 'entityUrn'] 
+LINKEDIN_URL = 'https://www.linkedin.com/in/'
+REGEX = /fs_salesProfile:\(([^,]+)/
+OUT_FILE = 'prospects.csv'
+OUT_HEADERS = [KEYS[1], KEYS[3], KEYS[4], KEYS[5]]
 
-# getting prospects
-prospects = Dir.glob(json_files_filter).flat_map do |file|
-  JSON.parse(File.read(file))[hash_keys[0]].map do |element|
-    current_position = element[hash_keys[2]].first
-    match = element[hash_keys[5]].match(regex)
-    {
-      hash_keys[1] => element[hash_keys[1]],
-      hash_keys[3] => current_position[hash_keys[3]],
-      hash_keys[4] => current_position[hash_keys[4]],
-      hash_keys[5] => "#{linkedin_URL}#{match[1]}"
-    }
-  end
+def get_files
+  Dir.glob(JSON_FILES_FILTER)
 end
 
-out = File.open(out_file, 'w')
-out << prospects.first.keys.to_csv
-prospects.each { |prospect| out << prospect.values.to_csv }
-p prospects.count
+def get_elements(file)
+  JSON.parse(File.read(file))[KEYS[0]]
+end
 
-out.close
+def get_data(element)
+  current_position = element[KEYS[2]].first
+  match = element[KEYS[5]].match(REGEX)
+  {
+    KEYS[1] => element[KEYS[1]],
+    KEYS[3] => current_position[KEYS[3]],
+    KEYS[4] => current_position[KEYS[4]],
+    KEYS[5] => "#{LINKEDIN_URL}#{match[1]}"
+  }
+end
+
+File.open(OUT_FILE, 'w') do |out|
+  out << OUT_HEADERS.to_csv
+  get_files.flat_map(&method(:get_elements))
+           .map(&method(:get_data))
+           .each{ |prospect| out << prospect.values.to_csv}
+end
